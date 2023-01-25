15a5b8e1072d041b8c8e85c9adac4b9e5ea1ae0b
nirajmaharjan007 <niraj.mhrjn770@gmail.com>
2023-01-25 22:24:51 +0545

    snake updated

--- entity/snake.py
+++ entity/snake.py
@@ -1,65 +1,73 @@
 import pygame as pg
-import numpy as np
 
 
 class Snake:
-    # x = np.array([])
-    # y = np.array([])
-    x = y = 50
     __size = 30
     __direction = 'r'
-    __speed = 0.85
+    __speed = 1
+    __snake_position = [100, 50]
+    __snake_body = [[100, 50],
+                    [90, 50],
+                    [80, 50],
+                    [70, 50]
+                    ]
+    __length = 5
 
     def __init__(self, screen):
         self.screen = screen
 
     def __move(self):
         if self.__direction == 'l':
-            self.x -= self.__speed
+            self.__snake_position[0] -= self.__speed
 
         if self.__direction == 'r':
-            self.x += self.__speed
+            self.__snake_position[0] += self.__speed
 
         if self.__direction == 'u':
-            self.y -= self.__speed
+            self.__snake_position[1] -= self.__speed
 
         if self.__direction == 'd':
-            self.y += self.__speed
+            self.__snake_position[1] += self.__speed
+
+        self.__snake_body.insert(0, list(self.__snake_position))
 
     def __update(self):
-        if self.x > (800):
-            self.x = 0
+        if self.__snake_position[0] > 800:
+            self.__snake_position[0] = 0
 
-        elif self.x < 0:
-            self.x = 800
+        elif self.__snake_position[0] < 0:
+            self.__snake_position[0] = 800
 
-        elif self.y > 600:
-            self.y = 0
+        elif self.__snake_position[1] > 600:
+            self.__snake_position[1] = 0
 
-        elif self.y < 0:
-            self.y = 600
+        elif self.__snake_position[1] < 0:
+            self.__snake_position[1] = 600
 
         key = pg.key.get_pressed()
 
-        if key[pg.K_DOWN]:
-            if self.__direction != 'u':
-                self.__direction = 'd'
+        if key[pg.K_DOWN] and self.__direction != 'u':
+            self.__direction = 'd'
 
-        if key[pg.K_UP]:
-            if self.__direction != 'd':
-                self.__direction = 'u'
+        if key[pg.K_UP] and self.__direction != 'd':
+            self.__direction = 'u'
 
-        if key[pg.K_LEFT]:
-            if self.__direction != 'r':
-                self.__direction = 'l'
+        if key[pg.K_LEFT] and self.__direction != 'r':
+            self.__direction = 'l'
 
-        if key[pg.K_RIGHT]:
-            if self.__direction != 'l':
-                self.__direction = 'r'
+        if key[pg.K_RIGHT] and self.__direction != 'l':
+            self.__direction = 'r'
 
     def render(self):
-        self.__update()
+        green_color = (170, 200, 115)
+        green_color_two = (205, 235, 145)
+
         self.__move()
-        pg.draw.rect(self.screen, (150, 200, 0), pg.Rect(
-            round(self.x, 2), round(self.y, 2), self.__size, self.__size))
-        print(round(self.x, 2), round(self.y, 2))
+        self.__update()
+
+        for pos in self.__snake_body:
+            pg.draw.rect(self.screen, green_color_two,
+                         pg.Rect(pos[0], pos[1], self.__size, self.__size))
+
+        pg.draw.rect(self.screen, green_color, pg.Rect(
+            self.__snake_position[0], self.__snake_position[1], self.__size, self.__size))

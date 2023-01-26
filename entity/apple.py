35a3a4ced72dd36e4cdeed7a75f245c02d92083c
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2023-01-26 14:36:39 +0545

    Entity apple updated

--- entity/apple.py
+++ entity/apple.py
@@ -3,23 +3,17 @@ from random import randint
 
 
 class Apple:
-    __apple_x = __apple_y = 0
+    fruit_spawn = True
+
+    apple_x = apple_y = 0
     __size = 12
 
     def __init__(self, screen):
         self.screen = screen
-        self.__apple_y = randint(1, 800)
-        self.__apple_x = randint(1, 600)
+        self.apple_y = randint(1, 800)
+        self.apple_x = randint(1, 600)
 
     def render(self):
         color = (235, 69, 95)
-        pg.draw.circle(self.screen, color, (self.__apple_x,
-                       self.__apple_y), self.__size)
-
-    @staticmethod
-    def get_x(self):
-        return self.__apple_x
-
-    @staticmethod
-    def get_y(self):
-        return self.__apple_y
+        pg.draw.circle(self.screen, color, (self.apple_x,
+                       self.apple_y), self.__size)

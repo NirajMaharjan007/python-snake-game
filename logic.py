15916d6e8e3d88cc314251151b40a373019859d7
nirajmaharjan007 <niraj.mhrjn770@gmail.com>
2023-01-26 22:42:38 +0545

    Modified and updated

--- logic.py
+++ logic.py
@@ -1,14 +1,26 @@
+import pygame as pg
 from entity.snake import Snake
 from entity.apple import Apple
 
 
 class Logic:
-
     def __init__(self, screen):
         self.screen = screen
-        self.snake = Snake(self.screen)
-        self.apple = Apple(self.screen)
+        self.score = 0
+        self.font = pg.font.SysFont("", 32)
+
+        global snake, apple
+        snake = Snake(self.screen)
+        apple = Apple(self.screen)
 
     def render(self):
-        self.snake.render()
-        # self.apple.render()
+
+        apple.render()
+        apple.collision(snake.get_x(), snake.get_y())
+        snake.render()
+
+        self.text = self.font.render(str(self.score), True, (255, 255, 255))
+        self.screen.blit(self.text, (10, 10))
+
+        if apple.get_collision():
+            self.score += 10
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from os import system
 from logic import Logic
 
 system("clear")
-system("python --version")
 print("Pygame version:", pg.__version__)
 
 pg.init()
+pg.font.init()
+
 screen = pg.display.set_mode((800, 600))
+clock = pg.time.Clock()
+
 pg.display.set_caption("My Game")
 
 running = True
@@ -15,6 +18,8 @@ running = True
 logic = Logic(screen)
 
 while running:
+    clock.tick(200)
+
     screen.fill((55, 70, 80))
     for event in pg.event.get():
         if event.type == pg.QUIT:
